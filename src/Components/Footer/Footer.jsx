ad33c4ea5b7a55cb43a44c378aa8d0510ed99417
unknown <designer.brightensolutions@gmail.com>
2024-10-22 14:57:00 +0530

    footer content css modified

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,49 +1,58 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {logo} from '../Images/Image'
+import { logo } from '../Images/Image';
 
 const Footer = () => {
     return (
         <div>
-            <footer className='mt-10 p-5 text-white text-center bottom-0 w-[100%] lg:bottom-0 bg-gradient ' >
+            <footer className='mt-10 p-5 text-white text-center bottom-0 w-[100%] lg:bottom-0 bg-gradient'>
                 <div className='flex sm:flex-row justify-start flex-col gap-[15px] sm:gap-0 sm:justify-between items-start sm:items-center container'>
                     <div>
-                      
-                        <img src={logo} alt=""  className='green-image w-16 mb-4    '/> 
-                        
-                        <p className='mb-1'>Lorem ipsum dolor sit amet. <br />Lorem ipsum dolor sit amet. <br />Lorem ipsum dolor sit amet.</p>
+                        <img src={logo} alt="Company Logo" className='green-image w-16 mb-4' />
+                        <p className='mb-1 w-80 text-start'>
+                            Discover the freshest and finest quality produce delivered right to your doorstep.
+                             Select from an extensive variety of vegetables at unbeatable prices.
+                             We prioritize your satisfaction above all else!
+                        </p>
                         <div className='flex gap-x-3 mt-3'>
-                            <Link to="" className='text-[29px] hover:text-gray-200 transition duration-300'><i className="fa fa-facebook-official" aria-hidden="true"></i></Link>
-                            <Link to="" className='text-[29px] hover:text-gray-200 transition duration-300'><i className="fa fa-twitter" aria-hidden="true"></i></Link>
-                            <Link to="" className='text-[29px] hover:text-gray-200 transition duration-300'><i className="fa fa-linkedin-square" aria-hidden="true"></i></Link>
+                            <Link to="" className='text-[29px] hover:text-gray-200 transition duration-300'>
+                                <i className="fa fa-facebook-official" aria-hidden="true"></i>
+                            </Link>
+                            <Link to="" className='text-[29px] hover:text-gray-200 transition duration-300'>
+                                <i className="fa fa-twitter" aria-hidden="true"></i>
+                            </Link>
+                            <Link to="" className='text-[29px] hover:text-gray-200 transition duration-300'>
+                                <i className="fa fa-linkedin-square" aria-hidden="true"></i>
+                            </Link>
                         </div>
                     </div>
 
+
                     <div className='flex flex-col items-start'>
-                        <h2 className='sm:text-lg text-[22px] font-bold mb-2'>Policies</h2>
+                        <h2 className='sm:text-lg text-[22px] font-bold mb-2'>Our Policies</h2>
                         <div>
                             <ul className='flex flex-col gap-y-2 text-start'>
+                                <li className='hover:text-gray-200 transition duration-200 cursor-pointer'>Delivery & Shipping</li>
+                                <li className='hover:text-gray-200 transition duration-200 cursor-pointer'>Return & Refund Policy</li>
+                                <li className='hover:text-gray-200 transition duration-200 cursor-pointer'>Privacy Policy</li>
                                 <li className='hover:text-gray-200 transition duration-200 cursor-pointer'>Terms & Conditions</li>
-                                <li className='hover:text-gray-200 transition duration-200 cursor-pointer'>Return Policy</li>
-                                <li className='hover:text-gray-200 transition duration-200 cursor-pointer'>Accessibility</li>
-                                <li className='hover:text-gray-200 transition duration-200 cursor-pointer'>Privacy</li>
                             </ul>
                         </div>
                     </div>
 
-                    <div className='flex flex-col items-start mt-[10px]  '>
-                        <h2 className='sm:text-lg text-[22px] font-bold mb-2 '>Our Info</h2>
+                    <div className='flex flex-col items-start mt-[10px]'>
+                        <h2 className='sm:text-lg text-[22px] font-bold mb-2'>Contact Us</h2>
                         <div className='flex flex-col gap-y-2 text-start'>
                             <p className='hover:text-gray-200 transition duration-200 cursor-pointer'>+91 9974870436</p>
-                            <p className='hover:text-gray-200 transition duration-200 cursor-pointer'>Kevdiveggies@gmail.com</p>
-                            <p className='hover:text-gray-200 transition duration-200 cursor-pointer'>123, New Street, USA.</p>
+                            <p className='hover:text-gray-200 transition duration-200 cursor-pointer'>support@veggieshop.com</p>
+                            {/* <p className='hover:text-gray-200 transition duration-200 cursor-pointer'>123, Fresh Street, New York, USA</p> */}
                         </div>
                     </div>
                 </div>
 
                 <div className='border-t-[1px] mt-[15px] pt-[15px]'>
                     <Link to="https://brightensolutions.com/" className='sm:text-[18px] text-[14px] font-roboto hover:text-gray-200 transition duration-300'>
-                        <p>Copyright © 2016-24 brighten solutions | All Right Reserved</p>
+                        <p>Copyright © 2016-24 Veggie Shop | All Rights Reserved</p>
                     </Link>
                 </div>
             </footer>
--- src/Components/ProductPage/ProductPage.jsx
+++ src/Components/ProductPage/ProductPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Navbar from '../Navbar';
 import Footer from '../Footer/Footer';
 import ButtonElement from '../Elements/ButtonElement';
@@ -7,13 +7,12 @@ import WhatsappIcon from '../Elements/WhatsappIcon';
 
 const ProductPage = () => {
     const { state } = useLocation();
+    const whatsappLink = `https://www.whatsapp.com/`
     return (
         <div className="bg-gray-100">
             <Navbar />
             <div className="mx-5 pt-20">
-            <div className="container py-7 px-5 flex flex-col md:flex-row bg-white p-6 mt-10 rounded-lg shadow ">
-               
-
+                <div className="container py-7 px-5 flex flex-col md:flex-row bg-white p-6 mt-10 rounded-lg shadow ">
                     <div className="md:w-1/2 overflow-hidden">
                         <img
                             src={state.image}
@@ -22,23 +21,25 @@ const ProductPage = () => {
                         />
                     </div>
                     <div className="md:w-1/2 md:pl-10 flex flex-col justify-around">
-                        <h1 className="text-4xl font-bold text-gray-800 mb-2 font-poppins  mt-4">{state.name}</h1>
+                        <h1 className="text-4xl font-semibold text-seconderyGreen mb-2 font-poppins  mt-4">{state.name}</h1>
                         <p className="text-lg text-gray-600  mb-4 ">{state.info}</p>
                         <div className="">
-                            <ul className="list-disc list-inside text-lg text-gray-600 space-y-2  mb-4">
+                            <ul className=" list-inside text-lg text-gray-600 space-y-2  mb-4">
                                 <li>✓ Free Worldwide Shipping</li>
                                 <li>✓ 30 Days Return</li>
                                 <li>✓ Member Discount</li>
                             </ul>
                         </div>
                         <p className="text-2xl font-semibold text-seconderyGreen mb-8">{state.price}</p>
-                        <ButtonElement text='Buy Now' />
+
+                        <ButtonElement text='Buy Now' onClick={() => window.open(whatsappLink)} />
+
                     </div>
                 </div>
             </div>
             <div className="my-16 mx-5 ">
                 <div className="container bg-white p-6 shadow rounded-lg">
-                    <h2 className="text-2xl font-bold text-gray-800 mb-4">Why Choose Our Products?</h2>
+                    <h2 className="text-2xl font-semibold text-seconderyGreen mb-4">Why Choose Our Products?</h2>
                     <ul className="list-disc list-inside text-lg text-gray-600 space-y-2">
                         <li>100% Organic Food from Farm Hong Quat Packaging</li>
                         <li>100% Organic Food</li>
@@ -47,7 +48,7 @@ const ProductPage = () => {
                 </div>
             </div>
             <Footer />
-            <WhatsappIcon/>
+            <WhatsappIcon />
         </div>
     );
 }
